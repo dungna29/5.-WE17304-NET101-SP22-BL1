0d1dfd8fa4041697a1c14f14760de45a49c31301
Dungna89 <dungna29@fpt.edu.vn>
2022-01-04 18:19:47 +0700

    1

---
+++ 5_WE17304_NET101_SP22_BL1/BAI_1.1_LamQuenVoiCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI_1._1_LamQuenVoiCSharp
+{
+    internal class Program
+    {
+        /*
+         * Bài 1: Làm quen với C# 1
+         *
+         * Các phím tắt cơ bản:
+         *      1. Ctrl + F5 hoặc F5 thì phải có câu lệnh Console.Readkey() - Chạy chương trình
+         *      2. Cw + Tab: Gọi nhanh Console.WriteLine();
+         *      3. Ctrl + K + C: Comment 1 dòng code hoặc 1 đoạn code
+         *      4. Ctrl + K + U: mở comment cũng tương tự
+         *      5. Ctrl + K + D: Format code giúp clean code hơn
+         *      6. Ctrl + Space: Gợi ý code
+         *      7. Ctrl + D: Nhân bản code
+         */
+        static void Main(string[] args)
+        {
+            //Hỗ trợ hiển thị tiếng việt
+            Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
+
+            //In ra màn hình 
+            Console.WriteLine("Chào các bạn học môn C#1 2022");
+            Console.WriteLine("Chúc các học tốt");
+           
+            /*
+             * 2 DẠNG NGƯỜI HỌC LẬP TRÌNH:
+             *      - 1. Chép code
+             *      - 2. Code
+             */
+            Console.WriteLine();
+        }
+    }
+}
