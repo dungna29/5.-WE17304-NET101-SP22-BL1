cc85f380b05bd7e01fd19cf844046d549d4a5877
Dungna89 <dungna29@fpt.edu.vn>
2022-01-13 18:20:20 +0700

    1

--- 5_WE17304_NET101_SP22_BL1/Bai_2.1_Array/Program.cs
+++ 5_WE17304_NET101_SP22_BL1/Bai_2.1_Array/Program.cs
@@ -38,7 +38,98 @@ namespace Bai_2._1_Array
         */
         static void Main(string[] args)
         {
-            int a = 5;
+            Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
+            //Phần 1: Khai báo biến
+            //<Kiểu dữ liệu> <Tên biến> = <Giá trị>;
+            int a = 10;//Khai báo 1 biến a có kiểu dữ liệu là số nguyên và có khởi tạo giá trị ban đầu
+            a = 5;
+
+            //Phần 2: Khai báo 1 mảng phải đặt tên arr
+            int[] arrNumber;//Khai báo 1 mảng số nguyên
+            //Muốn dùng mảng bắt buộc phải khởi tạo
+            arrNumber = new int[5];//Khởi tạo 1 mảng số nguyên có kích thước là 5 phần tử
+
+            string[] arrString = new string[3];//Khởi tạo 1 mảng String gồm 3 phần tử
+            double[] arrDoubles = { 8.6, 9.2, 5.6, 4.3 };//Khởi tạo 1 mảng có 4 phần tử bên trong.
+            double[] arrDoubles1 = new double[] { 8.6, 9.2, 5.6, 4.3 };
+
+            var arrDoubles2 = new double[5];//Khai báo ngầm định thì lúc này biến arrDoubles2 nó là 1 mảng số thực
+
+            //Phần 3: Gán giá trị cho mảng cần quan tâm đến INDEX
+            //arrNumber = 5; Sai vì không xác định được vị trí cần lưu vào
+            arrNumber[0] = 5;//Gán giá trị vào index 0
+            arrNumber[1] = 15;
+            arrNumber[2] = 25;
+
+            //Phần 4: Truy xuất giá trị trong mảng cần biết INDEX (Lưu vào đâu thì lấy ra ở đó)
+            Console.WriteLine(arrNumber[0]);//=5
+            Console.WriteLine(arrNumber[1]);
+            Console.WriteLine(arrNumber[2]);
+            Console.WriteLine(arrNumber[3]);
+            Console.WriteLine(arrNumber[4]);
+            //Console.WriteLine(arrNumber[5]);// IndexOutOfRangeException
+
+            //Sử dụng for để in các phần tử trong mảng
+            //arrNumber.Length = kích thước của mảng và là số nguyên
+            for (int i = 0; i < arrNumber.Length; i++)
+            {
+                Console.WriteLine(arrNumber[i]);
+            }
+
+            //fore + tab = Vòng lặp foreach dùng để in tuần tự
+            foreach (int x in arrNumber)
+            {
+                Console.WriteLine(x);
+            }
+            /*
+        Bài 1: Viết 1 chương trình cho phép người dùng nhập vào bao nhiêu số tùy thích. Sau khi nhập xong in ra màn hình các số chẵn trên 1 dòng và số lẻ trên 1 dòng.
+        
+        Bài 2: Viết 1 chương trình cho phép người dùng nhập vào 1 dãy điểm. In ra màn hình dãy điểm người vừa nhập và bệnh cạnh mỗi 1 đầu điểm thông báo Trượt hoặc đỗ dựa trên điều kiện điểm đỗ >=5 và Trượt <5.
+        */
+            //Giải bài tập 1:
+            int[] arrNumberB1;
+            int size;
+            Console.Write("Mời bạn nhập số lượng: ");
+            size = Convert.ToInt32(Console.ReadLine());
+            //Phải khởi tạo mảng theo kích thước người dùng nhập vào
+            arrNumberB1 = new int[size];
+            for (int i = 0; i < arrNumberB1.Length; i++)
+            {
+                Console.Write($"Mời bạn nhập index thứ {i}: ");
+                arrNumberB1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Các số chẵn: ");
+            // foreach (var x in arrNumberB1)
+            // {
+            //     if (x%2==0)
+            //     {
+            //         Console.Write(x + " ");
+            //     }
+            //
+            //     // Console.WriteLine(x + (x >= 5 ? "Qua môn" : "Học lại")); Cách giải bài 2 nhanh
+            // }
+
+            int index = 0;
+            for (int i = 0; i < arrNumberB1.Length * 2; i++)
+            {
+                if (i <= arrNumberB1.Length - 1)
+                {
+                    Console.Write(((arrNumberB1[i] % 2 == 0) ? Convert.ToString(arrNumberB1[i]) : "") + " ");
+                }
+                if (i == arrNumberB1.Length - 1)
+                {
+                    Console.WriteLine();
+                }
+                if (i > arrNumberB1.Length - 1)
+                {
+                   
+                    Console.Write(((arrNumberB1[index] % 2 != 0) ? Convert.ToString(arrNumberB1[index]) : "") + " ");
+                    index++;
+                }
+            }
+
+            
         }
     }
 }
---
+++ 5_WE17304_NET101_SP22_BL1/Bai_2.2_ArrayThuatToanNoiBot/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_2._2_ArrayThuatToanNoiBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
+            //1. Viết ra giấy trên từng lần chạy hoặc word hoặc excel
+            //2. 1 Cách thông minh hơn
+            int[] arrNumbers = { 9, 8, 7, 6, 5 };
+            foreach (var x in arrNumbers)
+            {
+                Console.Write(x + " ");
+            }
+            /*
+             * 9, 8, 7, 6, 5
+             * Vòng 1 - i = 0; 
+             * index[i] = 9 > index[j]1 = 8
+             * 8, 9, 7, 6, 5
+             * index[i] = 8 > index[j]2 = 7
+             * 7, 9, 8, 6, 5
+             * index[i] = 7 > index[j]3 = 6
+             * 6, 9, 8, 7, 5
+             * index[i] = 6 > index[j]4 = 5
+             * 5, 9, 8, 7, 6
+             * Vòng 2 - i = 1;
+             * index[i] = 9 > index[j]2 = 8
+             * 5, 8, 9, 7, 6
+             * index[i] = 8 > index[j]3 = 7
+             * 5, 7, 9, 8, 6
+             * index[i] = 7 > index[j]4 = 6
+             * 5, 6, 9, 8, 7
+             * Vòng 3 - i = 2;
+             * index[i] = 9 > index[j]3 = 8
+             * 5, 6, 8, 9, 7
+             * index[i] = 8 > index[j]4 = 7
+             * 5, 6, 7, 9, 8
+             * Vòng 4 - i = 3;
+             * index[i] = 9 > index[j]4 =8
+             * 5, 6, 7, 8, 9
+             */
+            // for (int i = 0; i < arrNumbers.Length - 1; i++)
+            // {
+            //     for (int j = i + 1; j < arrNumbers.Length; j++)
+            //     {
+            //         if (arrNumbers[i] > arrNumbers[j])
+            //         {
+            //             int temp = arrNumbers[i];
+            //             arrNumbers[i] = arrNumbers[j];
+            //             arrNumbers[j] = temp;
+            //         }
+            //     }
+            // }
+
+            for (int i = 0; i < arrNumbers.Length - 1; i++)
+            {
+                Console.WriteLine("Lần chay thứ: " + i);
+                foreach (var x in arrNumbers)
+                {
+                    Console.Write(x + " ");
+                }
+                Console.WriteLine("");
+                for (int j = i + 1; j < arrNumbers.Length; j++)
+                {
+                    Console.Write("Điều kiện {0} > {1} \n", arrNumbers[i], arrNumbers[j]);
+                    if (arrNumbers[i] > arrNumbers[j])
+                    {
+                        int temp = arrNumbers[i];
+                        arrNumbers[i] = arrNumbers[j];
+                        arrNumbers[j] = temp;
+                        Console.Write("Hoán vị của:{0} & {1} \n", arrNumbers[i], arrNumbers[j]);
+                        foreach (int x in arrNumbers)
+                        {
+                            Console.Write(x + " ");
+                        }
+                        Console.WriteLine("");
+                    }
+                }
+            }
+            Console.WriteLine();
+            foreach (var x in arrNumbers)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
